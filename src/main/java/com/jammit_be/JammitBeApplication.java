56affb4402a6ca01bfce2a475266593d8d68dcca
junghunee <cjswovkdnj12@naver.com>
2025-05-18 20:23:42 +0900

    init

---
+++ src/main/java/com/jammit_be/JammitBeApplication.java
@@ -0,0 +1,13 @@
+package com.jammit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JammitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JammitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jammit_be/JammitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jammit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JammitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
